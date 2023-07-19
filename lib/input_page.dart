16a3494cec038e9e18ca1bafe1c35ec9b0a71d66
deryasevincok <d.sevincok@hotmail.com>
2023-07-19 16:36:55 +0300

    deneme2

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+//import 'package:flutter/rendering.dart';
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
