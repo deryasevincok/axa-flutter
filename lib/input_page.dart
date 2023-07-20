6bb926a2b150af1f456a4c61e4eb995c9a4bf85e
deryasevincok <d.sevincok@hotmail.com>
2023-07-20 08:48:01 +0300

    deneme

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter/rendering.dart';
+import 'dart:math' as math;
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -11,56 +12,62 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Stack(   //Container üstüne Text ve Card widgetlerı koyuldu Stack sayesinde
-        children: <Widget>[
-          Scaffold(
-            body: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/galata.jpg"),
-                  fit: BoxFit.cover,  //ekran dolduruldu .jpg ile
-                ),
+    return Stack(
+      //Container üstüne Text ve Card widgetlerı koyuldu Stack sayesinde
+      children: <Widget>[
+        Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/galata.jpg"),
+                fit: BoxFit.cover, //ekran dolduruldu .jpg ile
               ),
-              padding: EdgeInsets.all(10.0),
-              child: SafeArea(
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    //crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      //RotationTransition(turns: AlwaysStoppedAnimation(15/360)),
-                      Text(
+            ),
+            padding: EdgeInsets.all(10.0),
+            child: SafeArea(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    //RotationTransition(turns: AlwaysStoppedAnimation(15/360)),
+
+                    Transform.rotate(
+                      angle: -math.pi / 4,
+                      child: Text(
                         "AXA",
                         style: TextStyle(
                           fontSize: 200.0,
                           color: Colors.white,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(100.0),
-                          ),
-                          color: Colors.red,
-                          child: SizedBox(
-                            width: 300,
-                            height: 50,
-                            child: Center(
-                              child: Text(
-                                'GET STARTED',
-                              ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100.0),
+                        ),
+                        color: Colors.red,
+                        child: SizedBox(
+                          width: 300,
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                              'GET STARTED',
                             ),
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-        ],
-      );
+        ),
+      ],
+    );
   }
 }
